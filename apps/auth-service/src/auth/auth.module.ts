8459fc647e234734aa8aeb309c0b10339e463612
Do Duy Tung <a01672034597@gmail.com>
2026-05-11 22:27:05 +0700

    hot fix docker compose

--- apps/auth-service/src/auth/auth.module.ts
+++ apps/auth-service/src/auth/auth.module.ts
@@ -5,10 +5,12 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { PrismaModule } from '../common/prisma/prisma.module';
+import { MailModule } from '../common/mail/mail.module';
 
 @Module({
   imports: [
     PrismaModule,
+    MailModule,
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'super-secret-key',
